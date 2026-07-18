94a5d63f10bdd25c41b687adad7730ed62c2cd74
crumberry <65675645+crumberry@users.noreply.github.com>
2026-07-18 14:51:09 +0000

    fix: relative CSS paths with base tag for project site

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes",
     },
+    pathPrefix: "",
   };
 }
